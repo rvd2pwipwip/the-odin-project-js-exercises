6304901f88371a29447cd307f63737dd250f3cf7
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-06-17 11:50:51 -0400

    Pass tests for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  // elements of array pass the filter if they are not included in args
+  return array.filter(element => !args.includes(element));
 };
 
 // Do not edit below this line
