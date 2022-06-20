078d87afcd6397c746573a7ec63484242440b349
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-06-20 08:09:06 -0400

    Pass all tests for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // create an array of lower case characters from the string argument
+  // and filter any non alphabetical character
+  const alpha = [...string.toLowerCase()].filter((char) =>
+    /[a-zA-Z]/.test(char)
+  );
+  // copy and reverse the array of characters
+  const reverse = alpha.slice().reverse();
 
+  // compare both arrays
+  // const equals = (a, b) => a.every((v, i) => v === b[i]);
+  // return equals(alpha, reverse);
+
+  return alpha.every((char, i) => char === reverse[i]);
 };
 
 // Do not edit below this line
