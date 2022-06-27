cfdf1b4b6e5590238581a09335ce4ba70395e8a7
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-06-27 08:25:44 -0400

    Pass tests for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // calculate the age of each person
+  let ages = people.map(
+    (p) =>
+      (p.yearOfDeath ? p.yearOfDeath : new Date().getFullYear()) - p.yearOfBirth
+  );
+  // get the index of the largest age
+  let i = ages.indexOf(Math.max(...ages));
+  // return the person object at the index of the largest age
+  return people[i];
 };
 
 // Do not edit below this line
