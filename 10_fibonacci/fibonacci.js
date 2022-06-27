9671e1dc78d610a3d6bc2dbeec6fe5629a9670f3
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-06-27 06:28:42 -0400

    Pass tests for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return 'OOPS';
+  }
+  let first = 0;
+  let second = 1;
+  for (let i = 1; i < num; i++) {
+    const sum = first + second;
+    first = second;
+    second = sum;
+  }
+  return second;
 };
 
 // Do not edit below this line
