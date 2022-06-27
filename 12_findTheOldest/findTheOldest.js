65a12f1be1f50a21f8fd62e4326799a1531afcb7
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-06-27 08:29:52 -0400

    course solution in comments

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -10,5 +10,22 @@ const findTheOldest = function (people) {
   return people[i];
 };
 
+// course solution:
+
+// const findTheOldest = function(array) {
+//   return array.reduce((oldest, currentPerson) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+//     return oldestAge < currentAge ? currentPerson : oldest;
+//   })
+// };
+
+// const getAge = function(birth, death) {
+//   if (!death) {
+//     death = new Date().getFullYear();
+//   }
+//   return death - birth;
+// };
+
 // Do not edit below this line
 module.exports = findTheOldest;
