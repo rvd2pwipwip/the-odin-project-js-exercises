a998b57a54684e6957b567d2f7b94f82a30de12f
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-19 06:22:51 -0400

    Start implementing the Caesar cipher

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,21 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+  const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+  const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let coded = str.split('');
 
+  for (let i = 0; i < coded.length; i++) {
+    if (UPPER.split('').includes(coded[i])) {
+      coded[i] = UPPER.split('').indexOf(coded[i] + shift);
+    }
+  }
+
+  return coded;
+
+  //check each char to be an alpha char
+  // return str.split('').filter((c) => /[a-zA-Z]/.test(c));
 };
 
 // Do not edit below this line
 module.exports = caesar;
+
+//.split('')
